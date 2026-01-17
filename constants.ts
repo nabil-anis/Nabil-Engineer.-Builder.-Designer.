f4c9baf30e16a6158027c59005f530e6bee3771f
Nabil Anis <202440098+nabil-anis@users.noreply.github.com>
2026-01-18 01:23:03 +0500

    Fix image path for uni22.jpg
    Updated image path for 'uni22.jpg' to include leading slash.

--- constants.ts
+++ constants.ts
@@ -8,7 +8,7 @@ export const ACHIEVEMENTS: Achievement[] = [
     date: '26 DECEMBER 2025',
     category: 'ENTREPRENEURSHIP',
     images: [
-      'uni22.jpg',
+      '/uni22.jpg',
       'https://images.unsplash.com/photo-1551288049-bbbda536339a?q=80&w=2340&auto=format&fit=crop',
       'https://images.unsplash.com/photo-1504868584819-f8e90526354c?q=80&w=2340&auto=format&fit=crop'
     ],
