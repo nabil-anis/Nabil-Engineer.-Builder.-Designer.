c8f3a25d61bb9fa0a031f2ea1490479d7f330d08
Nabil Anis <202440098+nabil-anis@users.noreply.github.com>
2026-01-19 02:08:49 +0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ export const ACHIEVEMENTS: Achievement[] = [
   {
      id: 'unifolio-poster',
     title: 'Unifolio: The Analytical Handshake',
-    description: 'Final Entrepreneurship Project Poster Presentation''Final Entrepreneurship Project Poster Presentation',
+    description: ' Entrepreneurship Course Final Project Poster Presentation',
     date: '26 DECEMBER 2025',
     category: 'ENTREPRENEURSHIP',
     images: [
